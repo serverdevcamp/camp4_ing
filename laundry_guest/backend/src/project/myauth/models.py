98e966907b0da53cf80af806a728795fe30fe79f
minsung <rkdalstjd9@naver.com>
2020-01-28 10:45:56 +0900

    guest 기본세팅 및 mysql 연동

---
+++ laundry_guest/backend/src/project/myauth/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=11)
+    business_num = models.CharField(max_length=10)
