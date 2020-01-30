3ab12a8f00cc7e96f372e5dd6df53bb3169a5a2b
minsung <rkdalstjd9@naver.com>
2020-01-30 17:09:26 +0900

    UserManager 수정

--- laundry_guest/backend/src/project/myauth/models.py
+++ laundry_guest/backend/src/project/myauth/models.py
@@ -3,29 +3,36 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=20)
-    address = models.CharField(max_length=100)
-    phone = models.CharField(max_length=11)
-    business_num = models.CharField(max_length=10)
+# class Profile(models.Model):
+#     user = models.OneToOneField(
+#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     nickname = models.CharField(max_length=20)
+#     address = models.CharField(max_length=100)
+#     phone = models.CharField(max_length=11)
+#     business_num = models.CharField(max_length=10)
 
-    def __str__(self):
-        return self.nickname
+#     def __str__(self):
+#         return self.nickname
 
 
 class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def create_user(self, email, username, password=None):
+    def create_user(self, username, email, password=None, nickname, address, detail_address, phone, status, role, business_num=0):
 
         if not email:
             raise ValueError('must have user email')
         user = self.model(
+            username=username,
             email=self.normalize_email(email),
-            username=username
+            nickname=nickname,
+            address=address,
+            detail_address=detail_address,
+            phone=phone,
+            status=status,
+            role=role,
+            business_num=business_num
         )
         user.set_password(password)
         user.save(using=self._db)
@@ -38,53 +45,53 @@ class UserManager(BaseUserManager):
             username=username,
             password=password
         )
-        user.is_admin = True
-        user.is_superuser = True
-        user.is_staff = True
+        user.role = "10"
         user.save(using=self._db)
         return user
 
 
-# class MyUser(AbstractBaseUser, PermissionsMixin):
+class Profile(AbstractBaseUser, PermissionsMixin):
 
-#     objects = UserManager()
+    objects = UserManager()
 
-#     STATUS_CHOICES = (
-#         ('0', '가입대기'),
-#         ('1', '가입활성화'),
-#         ('8', '블랙리스트'),
-#         ('9', '탈퇴')
-#     )
-#     ROLE_CHOICES = (
-#         ('0', '일반 유저'),
-#         ('5', '세탁소 사장'),
-#         ('10', '관리자')
-#     )
-#     username = models.CharField(
-#         max_length=16,
-#         unique=True
-#     )
-#     email = models.EmailField(
-#         max_length=50,
-#     )
-#     nickname = models.CharField(
-#         max_length=10,
-#         unique=True
-#     )
-#     address = models.CharField(max_length=50)
-#     detail_address = models.CharField(max_length=30)
-#     phone = models.CharField(max_length=11)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     last_login = models.DateTimeField(blank=True)
-#     status = models.CharField(max_length=2, choices=STATUS_CHOICES)
-#     role = models.CharField(max_length=2, choices=ROLE_CHOICES)
-#     business_num = models.CharField(max_length=10, unique=True)
+    STATUS_CHOICES = (
+        ('0', '가입대기'),
+        ('1', '가입활성화'),
+        ('8', '블랙리스트'),
+        ('9', '탈퇴')
+    )
+    ROLE_CHOICES = (
+        ('0', '일반 유저'),
+        ('5', '세탁소 사장'),
+        ('10', '관리자')
+    )
+    username = models.CharField(
+        max_length=16,
+        unique=True
+    )
+    email = models.EmailField(
+        max_length=50,
+    )
+    nickname = models.CharField(
+        max_length=10,
+        unique=True
+    )
+    address = models.CharField(max_length=50)
+    detail_address = models.CharField(max_length=30)
+    phone = models.CharField(max_length=11)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(blank=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES)
+    business_num = models.CharField(max_length=10, unique=True, blank=True)
 
-#     is_active = models.BooleanField(default=True)
-#     is_admin = models.BooleanField(default=False)
-#     is_superuser = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     date_joined = models.DateTimeField(auto_now_add=True)
-#     USERNAME_FIELD = 'username'
-#     REQUIRED_FIELDS = ['email']
+    @property
+    def is_staff(self):
+        return self.role == "10"
+
+    @property
+    def is_superuser(self):
+        return self.role == "10"
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
