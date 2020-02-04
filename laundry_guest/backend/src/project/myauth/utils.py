ecaaadaea3015734f99ae2454d9eaf61620d1538
minsung <rkdalstjd9@naver.com>
2020-02-04 19:00:38 +0900

    미들웨어에서 토큰 값 비교 후 user 인증

--- laundry_guest/backend/src/project/myauth/utils.py
+++ laundry_guest/backend/src/project/myauth/utils.py
@@ -10,37 +10,15 @@ from .models import Profile
 class MyTokenAuthenticationMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
-        self.API_URLS = [
-            re.compile(r'laundry/(.*)'),
-        ]
 
     def __call__(self, request):
-
-        path = request.path_info.lstrip('/')
-        valid_urls = (url.match(path) for url in self.API_URLS)
-        if not any(valid_urls):
-            response = self.get_response(request)
-            return response
-        key = request.COOKIES['jwt']
+        key = request.COOKIES.get('jwt')
         session_jwt = request.session.get(key)
         redis_jwt = cache.get(key)
-        if redis_jwt == None and session_jwt != None:
-            key = session_jwt.split('.')[2]
-            cache.set(key, session_jwt)
-
-        if session_jwt == redis_jwt:
+        if session_jwt != None and session_jwt == redis_jwt:
             payload = jwt.decode(session_jwt, settings.SECRET_KEY, 'HS256')
             username = payload['username']
             profile = Profile.objects.get(username=username)
             request.user = profile
-            response = self.get_response(request)
-            return response
-
-        else:
-            response = JsonResponse({
-                'response': 'error',
-                'message': '로그인이 필요합니다.'
-            }, status=200)
-            return response
         response = self.get_response(request)
         return response
