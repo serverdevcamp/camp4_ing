5cb51d33206985659f5e3fb015faef89c8ad5ff5
minsung <rkdalstjd9@naver.com>
2020-02-11 22:49:05 +0900

    header component 완성

--- laundry_guest/frontend/src/src/pages/LaundryListView.js
+++ laundry_guest/frontend/src/src/pages/LaundryListView.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from '../components/LaundryListView/LaundryListView.scss';
 import className from 'classnames/bind';
+import Header from '../components/Common/Header';
+
+const cx = className.bind(styles);
 
 class LaundryListView extends React.Component {
-    render() {
-        return (
-            <div>hello laundry list</div>
-        )
-    }
+  render() {
+    return (
+      <div className={cx('laundry-list-page')}>
+        <Header name={"세탁소 목록"} />
+      </div>
+
+    )
+  }
 }
 
 export default LaundryListView;
