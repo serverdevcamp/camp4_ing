827de690c0d93493624caf1ef374fb71d8407728
minsung <rkdalstjd9@naver.com>
2020-02-13 17:47:56 +0900

    OrderView 완성

--- laundry_guest/frontend/src/src/pages/OrderView.js
+++ laundry_guest/frontend/src/src/pages/OrderView.js
@@ -1,13 +1,88 @@
 import React from 'react';
 import styles from '../components/OrderView/OrderView.scss';
 import className from 'classnames/bind';
+import Header from '../components/Common/Header';
+import OrderItem from '../components/OrderView/OrderItem';
+import CustomButton from '../components/Common/CustomButton';
 
 const cx = className.bind(styles);
 
-class OrderView extends React.component {
+const orderItemData = [
+  {
+    id: 1,
+    category: "바지",
+    material: "면",
+    count: 5,
+    price: 2000,
+    requirement: "바지도 조심히 빨아주세요."
+  },
+  {
+    id: 2,
+    category: "셔츠",
+    material: "린넨",
+    count: 4,
+    price: 3000,
+    requirement: "린넨은 구김 펴주세요."
+  },
+  {
+    id: 3,
+    category: "바지",
+    material: "청",
+    count: 1,
+    price: 4000,
+    requirement: "청물 안빠지게 해주세요."
+  },
+]
+
+class OrderView extends React.Component {
   render() {
+
+    const { history } = this.props;
+    const laundryName = "스마일 세탁소";
+
+
+    const handleLaundryDetail = () => {
+      history.goBack();
+    }
+
+    const orderItemComponent = orderItemData.map(({ id, ...rest }) => {
+      console.log(rest);
+      return (
+        <OrderItem
+          key={id}
+          {...rest}
+        />
+      )
+    })
+
     return (
-      <div>hello</div>
+      <div className={cx('order-page')} >
+        <Header
+          name={'장바구니'}
+          handle={handleLaundryDetail}
+        />
+        <div className={cx('laundry-name-wapper')}>
+          {laundryName}
+        </div>
+        <div className={cx('order-item-wapper')}>{orderItemComponent}</div>
+        {/* TODO: 전체 금액 어떻게 계산하지? */}
+        <div className={cx('total-price-wapper')}>
+          <div>주문금액</div>
+          <div>13000원</div>
+        </div>
+        <div className={cx('sub-message')}>
+          LaundryRunner는 통신판매중개자이며 통신판매의 당사자가 아닙니다. 따라서 LaundryRunner는 상품, 거래 정보 및 거래에 책임을 지지 않습니다.
+        </div>
+        <CustomButton
+          type={'button'}
+          className={'order-button'}
+          value={'주문하기'}
+          isInActive={true}
+        />
+
+      </div>
+
+
     )
   }
 }
