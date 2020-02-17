e23fe57d2cb03553d631c5835c7b5cbf4c706916
minsung <rkdalstjd9@naver.com>
2020-02-17 15:57:13 +0900

    django credentials 설정

--- laundry_guest/backend/src/project/config/settings.py
+++ laundry_guest/backend/src/project/config/settings.py
@@ -197,7 +197,9 @@ REST_FRAMEWORK = {
 # }
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://127.0.0.1:3000'
-)
+CORS_ALLOW_CREDENTIALS = True
+
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000'
+# )
