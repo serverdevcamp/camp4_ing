38f4a3efad82eb40614c1b3f8ca67ae1e2b673e9
minsung <rkdalstjd9@naver.com>
2020-02-28 15:19:10 +0900

    세탁소 이미지 및 세탁소 아이템 이미지 업로드

--- laundry_guest/frontend/src/src/pages/LaundryListView.js
+++ laundry_guest/frontend/src/src/pages/LaundryListView.js
@@ -5,7 +5,12 @@ import Header from '../components/Common/Header';
 import Menu from '../components/Common/Menu';
 import Laundry from '../components/LaundryListView/Laundry';
 import axios from 'axios';
-import EndPoint from '../config/EndPoint';
+import laundryShopImg1 from '../static/image/laundryShop1.png';
+import laundryShopImg2 from '../static/image/laundryShop3.png';
+import laundryShopImg3 from '../static/image/laundryShop2.png';
+import laundryShopImg4 from '../static/image/laundryShop4.png';
+import laundryShopImg5 from '../static/image/laundryShop5.jpeg';
+import laundryShopImg6 from '../static/image/laundryShop6.jpeg';
 
 
 const cx = className.bind(styles);
@@ -26,8 +31,9 @@ const LaundryListView = ({ match, history }) => {
         }
       })
   }
-
-  const leftComponent = laundrys.map(({ id, name, information, grade, like_num }) => {
+  const laundryShopImgList = [laundryShopImg1, laundryShopImg2, laundryShopImg3, laundryShopImg4, laundryShopImg5, laundryShopImg6]
+  const leftComponent = laundrys.map(({ id, name, information, grade, like_num }, index) => {
+    console.log(index);
     return (
       <Laundry
         className={cx('laundry-item')}
@@ -38,6 +44,7 @@ const LaundryListView = ({ match, history }) => {
         grade={grade}
         likeNum={like_num}
         match={match}
+        laundryShopImg={laundryShopImgList[index]}
       />
     )
   })
